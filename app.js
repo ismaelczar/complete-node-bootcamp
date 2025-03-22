241d3e2bd9b7f26e17e34a0c4785846fcbecf598
ismael cezar <103972495+ismaelczar@users.noreply.github.com>
2025-03-21 21:35:02 -0300

    feat: adding file server.js

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const toursRouter = require('./routes/toursRoutes');
-const usersRouter = require('./routes/usersRouter')
+const morgan = require('morgan')
 
 const app = express();
 
+const toursRouter = require('./routes/toursRoutes');
+const usersRouter = require('./routes/usersRouter')
+
+
 app.use(express.json())
+app.use(morgan('dev'))
 app.use('/api/v1/tours', toursRouter)
 app.use('/api/v1/users', usersRouter)
 
-const port = 3000
-app.listen(port, () => {
-    console.log(`App running on port ${port}`)
-})
+module.exports = app
